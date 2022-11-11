55e3dc05c4609d939bfb397e44d68ae49e94610b
PeterHolmqvist <peterholmqvist@hotmail.com>
2022-11-11 11:25:36 +0000

    variable with object array key and value pair

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,3 +2,30 @@ let selectionButtons = document.querySelectorAll('[data-selection]');
 let finalColumn = document.querySelector('[data-final-column]');
 let computerScoreSpan = document.querySelector('[data-computer-score]');
 let yourScoreSpan = document.querySelector('[data-your-score]');
+let SELECTIONS = [{
+    name: 'paper', 
+    emoji: '📃',
+    beats: ['spock', 'rock']
+  },
+  {
+    name: 'rock',
+    emoji: '🪨',
+    beats: ['lizard', 'scissors']
+
+  },
+  {
+    name: 'scissors',
+    emoji: '✂',
+    beats: ['paper', 'lizard']
+  },
+  {
+    name: 'lizard',
+    emoji: '🦎',
+    beats: ['paper', 'spock']
+  },
+  {
+    name: 'spock',
+    emoji: '🖖',
+    beats: ['rock', 'scissors']
+  },
+]; 
