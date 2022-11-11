1be0c0f4f1c72e62e09c708da7462103f1bd790f
PeterHolmqvist <peterholmqvist@hotmail.com>
2022-11-11 11:21:01 +0000

    add variables to my data-selection element in JS

---
+++ assets/script/script.js
@@ -0,0 +1,4 @@
+let selectionButtons = document.querySelectorAll('[data-selection]');
+let finalColumn = document.querySelector('[data-final-column]');
+let computerScoreSpan = document.querySelector('[data-computer-score]');
+let yourScoreSpan = document.querySelector('[data-your-score]');
